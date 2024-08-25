a34e7b475e990bff68f3ddb7a4e7e73a6afc451c
Mohamed Ali <The_blue_Whale@Windowslive.com>
2024-08-25 22:30:41 +0300

    Django Views and URL Configuration

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from relationship_app.models import Book
-from django.views.generic import DetailView
-from relationship_app.models import Library
+from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 
