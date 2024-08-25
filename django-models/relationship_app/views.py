2d2149579c950c6a37c7ad16ce0645c768e507e0
Mohamed Ali <The_blue_Whale@Windowslive.com>
2024-08-25 22:55:31 +0300

    Django Views and URL Configuration

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,7 +5,7 @@ def list_books(request):
     books = Book.objects.all()  # Fetch all books from the database
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library  # Assuming you have a Library model in your app
 
 class LibraryDetailView(DetailView):
