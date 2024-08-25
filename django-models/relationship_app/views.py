8c2f96e249c6fbddb58e40588ba89441c255bfdb
Mohamed Ali <The_blue_Whale@Windowslive.com>
2024-08-25 22:54:17 +0300

    Django Views and URL Configuration

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
-from .models import Book
-from .models import Library
-from django.views.generic.detail import DetailView
+from .models import Book  # Assuming you have a Book model in your app
 
 def list_books(request):
-    books = Book.objects.all()
-    context = {'books': books}
-    return render(request, 'relationship_app/list_books.html', context)
+    books = Book.objects.all()  # Fetch all books from the database
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+from django.views.generic import DetailView
+from .models import Library  # Assuming you have a Library model in your app
 
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
 
+    # Optionally override get_context_data if you need to add more data to the context
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        library = self.get_object()
-        context['books'] = library.books.all()
         return context
