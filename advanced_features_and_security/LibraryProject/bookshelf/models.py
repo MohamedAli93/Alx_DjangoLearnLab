3deb19361e752ca31494b77e892a141964d79ed0
Mohamed Ali <The_blue_Whale@Windowslive.com>
2025-02-14 05:18:00 +0200

    Final commit2 - Django project

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 from django.contrib.auth.models import BaseUserManager
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
 # Create your models here.
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
