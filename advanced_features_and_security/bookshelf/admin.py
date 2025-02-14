67190a95e5473e6f733ba9bc9dbe61dd42a2d736
Mohamed Ali <The_blue_Whale@Windowslive.com>
2025-02-14 04:27:43 +0200

    Initial commit - Django project

---
+++ advanced_features_and_security/bookshelf/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Book
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'profile_photo', 'is_staff']
+    list_filter = ['is_staff', 'is_superuser', 'date_of_birth']
+    search_fields = ['username', 'email']
+    ordering = ['username']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author', 'publication_year')
+    list_filter = ("author", "publication_year")
+admin.site.register(Book, BookAdmin)
+# Register your models here.
