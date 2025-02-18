350399c1ed41482bbe69fc90d43d870ba0d663a0
Mohamed Ali <The_blue_Whale@Windowslive.com>
2025-02-18 03:59:23 +0200

    Added Book API endpoint

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework import generics  # Ensure this import is present
 from .models import Book
 from .serializers import BookSerializer
 
-# Create your views here.
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):  # Ensure this extends generics.ListAPIView
     queryset = Book.objects.all()
     serializer_class = BookSerializer
