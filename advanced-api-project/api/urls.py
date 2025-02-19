22cf4518a444336660147cbd255360f44a93a904
Mohamed Ali <The_blue_Whale@Windowslive.com>
2025-02-19 23:19:01 +0200

    Added custom views and permissions for CRUD operations

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
