631edafb6231b95ae25c269ec58af496abf0a179
Mohamed Ali <The_blue_Whale@Windowslive.com>
2025-02-19 23:32:10 +0200

    Added filtering, searching, and ordering to BookListView

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import permissions
@@ -16,6 +16,19 @@ class IsOwnerOrAdmin(permissions.BasePermission):
 
 # List all books
 class BookListView(generics.ListAPIView):
+    """
+    Allows users to filter, search, and order books based on different attributes.
+    Available filters:
+        - title
+        - author
+        - publication_year
+    Available search fields:
+        - title
+        - author
+    Available ordering:
+        - title
+        - publication_year
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
