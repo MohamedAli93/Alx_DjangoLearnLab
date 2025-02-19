25163ae83eaaac133421e3639b450832ce9ad182
Mohamed Ali <The_blue_Whale@Windowslive.com>
2025-02-19 23:45:58 +0200

    Added filtering, searching, and ordering to BookListView

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
-from django import rest_framework, DjangoFilterBackend
+from django_filters import rest_framework, DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import permissions
